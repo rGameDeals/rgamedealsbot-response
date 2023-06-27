da6d61520263805d20ab593bf906a97a43fc76b1
dgc1980 <dave@dgc1980.com>
2023-06-28 02:21:29 +1000

    added announcement option for posting comment via wikiconfig

--- bot/bot.py
+++ bot/bot.py
@@ -304,7 +304,7 @@ If this deal has been mistakenly closed or has been restocked, you can open it a
 
     logging.debug("posting reply")
     if post_footer:
-      if wikiconfig['announcement-enable'].lower() == 'true':
+      if wikiconfig['announcement-enable'] == 'true':
         reply_text = wikiconfig['announcement'] + "\n\n*****\n\n"+reply_text
       if reply_text != "":
         comment = submission.reply(body=reply_text+"\n\n*****\n\n"+footer)
@@ -313,7 +313,7 @@ If this deal has been mistakenly closed or has been restocked, you can open it a
       comment.mod.distinguish(sticky=True)
       logging.info("Replied to: " + submission.title + "   Reason: " + reply_reason)
     else:
-      if wikiconfig['announcement-enable'].lower() == 'true':
+      if wikiconfig['announcement-enable'] == 'true':
         reply_reason = "posting annoucement"
         postcomment = wikiconfig['announcement'] + "\n\n*****\n\n" + wikiconfig['announcement-only-footer']
         comment = submission.reply(body=postcomment)
